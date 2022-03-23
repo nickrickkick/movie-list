ef5a699a7602ad0d4f619d42ed32f2317eef9714
Michael Nowlin <michaelnowlin5@gmail.com>
2022-03-22 20:40:37 -0600

    core lab is completed!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const message = document.querySelector("#message");
 console.log("hello world")
 
 const addMovie = (event) => {
@@ -6,9 +7,28 @@ const addMovie = (event) => {
     const movie = document.createElement("li");
     const movieTitle = document.createElement("span");
     movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie);
     movie.appendChild(movieTitle);
+    deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
     document.querySelector("ul").appendChild(movie);
     inputField.value = "";
+}
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove() 
+    message.textContent = "Movie deleted!";
+}
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle("checked") 
+    if (event.target.classList.contains("checked")){
+        message.textContent = "Movie Watched!";
+    } else {
+        message.textContent = "Movie added back!";
+    }
 }
 
 document.querySelector("form").addEventListener("submit", addMovie);
